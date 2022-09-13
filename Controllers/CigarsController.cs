1172d171a6b27acf415a16e94cd41eb9ecbd6cec
JD Simmons <hippeejd@gmail.com>
2022-09-13 15:21:37 -0600

    setting up

---
+++ Controllers/CigarsController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CigarPalace.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CigarsController : ControllerBase
+    {
+        
+    }
+}
